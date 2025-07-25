8ecf0b2f7d3541d2e04cc39c4a967b573214b56b
sparsh912 <sparshmaheshwari0908@gmail.com>
2025-07-25 20:57:52 +0530

    api response and error handling

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json({limit: '16kb'}))
+// This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+// This is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.static("public"))
+// This is a built-in middleware function in Express. It serves static files and is based on serve-static
+
+app.use(cookieParser())
+
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERRR befor listening ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("MONGODB Connection Failed !!", error);
+    })
 
 
 
