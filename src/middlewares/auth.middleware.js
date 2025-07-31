ccc0d8879b250992d248f511ea6ed40673a330f5
sparsh912 <sparshmaheshwari0908@gmail.com>
2025-07-31 23:11:08 +0530

    debug

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
-import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 
-export const verifyJWT = asyncHandler( async (req ,res , next) => {
+export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken  || req.header("Authorization")?.replace("Bearer" , "")
     
@@ -12,7 +12,7 @@ export const verifyJWT = asyncHandler( async (req ,res , next) => {
             throw new ApiError(401 ,"Unauthorized request")
         }
     
-        const decodedToken = jwt.verify(token , process.env.AACCESS_TOKEN_SECRET)    //direct method ia given "verify"
+        const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)    //direct method ia given "verify"
     
         const user =await User.findById(decodedToken?._id).select("-password -refreshToken")
     
